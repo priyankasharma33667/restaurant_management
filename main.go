923a8dde3ac826822f81023bc8b2bc12ae21ed8b
Priyanka <priyankasharma33667@gmail.com>
2023-09-20 20:03:48 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import(
+	"os"
+	"github.com/gin-gonic/gin"
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/routes"
+	"golang-restaurant-management/middleware"
+	"go.mongodb.org/mongo-driver/mongo"
+
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main(){
+	port := os.Getenv("PORT")
+
+	if port == ""{
+		port = "8000"
+	}
+
+	router := gin.New{}
+	router.User(gin.Logger())
+	routes.UserRoutes(router)
+	routes.User(middleware.Authenication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+
+}
